75141b6b65ff92ecc8aa2a40ab018f0aeff538c6
Nicole Sanders <nsanders9022@gmail.com>
2017-05-05 10:03:59 -0700

    fix spelling of Available in Worker model

--- src/MrFixIt/Models/Worker.cs
+++ src/MrFixIt/Models/Worker.cs
@@ -15,14 +15,15 @@ namespace MrFixIt.Models
         public int WorkerId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public bool Avaliable { get; set; }
+        //Fixed spelling of available
+        public bool Available { get; set; }
         public string UserName { get; set; }
         //this comes from Identity.User
         public virtual ICollection<Job> Jobs { get; set; }
 
         public Worker()
         {
-            Avaliable = true;
+            Available = true;
         }
 
     }
