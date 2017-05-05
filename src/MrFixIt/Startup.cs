047204acfcfa7d720d93b20ac2027394a637f639
Nicole Sanders <nsanders9022@gmail.com>
2017-05-05 13:43:06 -0700

    add pending and completion status on the worker index page for each job they claimed

--- src/MrFixIt/Startup.cs
+++ src/MrFixIt/Startup.cs
@@ -25,8 +25,6 @@ namespace MrFixIt
                 .AddJsonFile("appsettings.json");
             Configuration = builder.Build();
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
@@ -39,7 +37,6 @@ namespace MrFixIt
                 .AddDefaultTokenProviders();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             app.UseStaticFiles();
@@ -57,7 +54,7 @@ namespace MrFixIt
             });
             app.Run(async (error) =>
             {
-                await error.Response.WriteAsync("You should not see this message. An error has occured.");
+                await error.Response.WriteAsync("An error has occured.");
             });
         }
     }
